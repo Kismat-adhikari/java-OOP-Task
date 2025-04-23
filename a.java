741307c0e71d61e8d6db22f40597c3667b693f38
Kismat-adhikari <kismatadhikari62@gmail.com>
2025-04-23 12:55:25 +0545

    question 1.

--- a.java
+++ a.java
@@ -1,19 +1,11 @@
-// multiplication tbale of a given number
-import java.util.Scanner;
+// Write a Java program to input your full name and print a greeting message.
 
-public class a{
+import java.util.Scanner;
+public class a {
     public static void main(String[] args){
         Scanner sc= new Scanner(System.in);
-        System.out.print("Enter a number for table:");
-        int num = sc.nextInt();
-        int i=1;
-        
-        
-        do{
-            System.out.println(num + "*" + i + "=" + (num*i));
-            i++;
-            
-        }while(i<=10);
-        }
-        
+        System.out.print("Enter your name");
+        String fullName=sc.nextLine();
+        System.out.println("Hello, " + fullName + "! Welcome to the Java program.");
     }
+}
