8b3f8dad0f6975d7ee808605f3144388afde6290
Kismat-adhikari <kismatadhikari62@gmail.com>
2025-04-23 13:04:00 +0545

    2nd ques

--- a.java
+++ a.java
@@ -1,11 +1,41 @@
-// Write a Java program to input your full name and print a greeting message.
+// 1Write a Java program to input your full name and print a greeting message.
 
-import java.util.Scanner;
-public class a {
-    public static void main(String[] args){
-        Scanner sc= new Scanner(System.in);
-        System.out.print("Enter your name");
-        String fullName=sc.nextLine();
-        System.out.println("Hello, " + fullName + "! Welcome to the Java program.");
-    }
-}
+// import java.util.Scanner;
+// public class a {
+//     public static void main(String[] args){
+//         Scanner sc= new Scanner(System.in);
+//         System.out.print("Enter your name");
+//         String fullName=sc.nextLine();
+//         System.out.println("Hello, " + fullName + "! Welcome to the Java program.");
+//     }
+// }
+
+
+// 2Take two numbers as input and print their sum, difference, product, and quotient.
+// import java.util.Scanner;
+// public class a{
+//     public static void main(String[] args) {
+//         Scanner sc= new Scanner(System.in);
+//         System.out.print("Enter the first number:");
+//         double num1 = sc.nextDouble();
+//         System.out.print("Enter the second number:");
+//         double num2 = sc.nextDouble();
+
+//         double sum = num1+ num2;
+//         double diff = num1-num2;
+//         double product = num1*num2;
+//         double quotient = num1/num2;
+
+//         System.out.println("The sum is " + sum);
+//         System.out.println("The diff is"+ diff);
+//         System.out.println("The product is"+ product);
+//         System.out.println("The quotient is"+quotient);
+
+
+
+//     }
+// }
+
+
+
+
